c85420189c2f9e217d39b50f9dcc0d1c6755a246
Sam Spilsbury <smspillaz@gmail.com>
2013-07-15 18:48:09 -0700

    Added support for pointer events and hide the system pointer.
    Created classes xbmc::wayland::Pointer to wrap wl_pointer and
    xbmc::PointerProcessor implementing xbmc::wayland::PointerListener
    which contains the logic for converting wayland button and pointer
    events into xbmc events.
    
    Made xbmc::wayland::Seat wait for a pointer to appear and initialize
    a pointer in WaylandInput.
    
    Call wl_pointer_set_cursor as soon as the pointer enters our surface
    as it is undefined before that.

--- xbmc/windowing/wayland/Seat.h
+++ xbmc/windowing/wayland/Seat.h
@@ -38,6 +38,10 @@ class IInputReceiver
 public:
 
   virtual ~IInputReceiver() {}
+
+  virtual bool InsertPointer(struct wl_pointer *pointer) = 0;
+
+  virtual void RemovePointer() = 0;
 };
 
 class Seat :
