58f631cdbd95c5b4de1be39f8d7757b35cbcde98
Sam Spilsbury <smspillaz@gmail.com>
2013-07-15 18:11:20 -0700

    Added initial support for displaying on Wayland compositors.
    SDL and X11 builds are disabled when using the --enable-wayland
    switch.
    
    EGLNativeTypeWayland is an implementation of EGLNativeType which
    connects to a running wayland compositor if possible and starts
    rendering in a new surface.
    
    WinEventsWayland polls the wayland display for new events and updates
    the main surface.
    
    The wayland client libraries are loaded dynamically and their functions
    should not be used directly. Instead, IDllWaylandClient should be injected
    into any objects that need to call functions in those libraries. Because
    the autogenerated protocol functions are inline in the header files
    and not available to be looked up, the protocol needs to be used directly.
    
    Some of the helpers there are:
    
     1. wayland::protocol::CreateWaylandObject -> creates a wl_proxy casted
        to the result type with a specific wl_interface as looked up in
        the client library.
     2. wayland::protocol::CallMethodOnWaylandObject -> calls wl_proxy_marshal
        with on the proxy with the specified opcode and arguments
     3. wayland::protocol::AddListenerOnWaylandObject -> adds a wl_listener
        to the wl_proxy
     4. wayland::protocol::DestroyWaylandObject -> calls wl_proxy_destroy
        on the proxy.
    
    There may be some cases where additional constructor or destructor functions
    might need to be called before or after CreateWaylandObject and
    DestroyWaylandObject. Developers should consult the autogenerated
    wayland-*-protocol.h headers to determine the expected call order.
    
    XBMCSurface and XBMCConnection wrap most of the wayland connection
    and surface creation / destruction and provide outputs for getting
    the current resolution and EGL surface.
    
    There is currently no support for keyboard input, mouse input or
    multiple monitors.

---
+++ xbmc/windowing/egl/EGLNativeTypeWayland.cpp
@@ -0,0 +1,287 @@
+/*
+ *      Copyright (C) 2011-2013 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+#define WL_EGL_PLATFORM
+ 
+#include <sstream>
+#include <iostream>
+#include <stdexcept>
+
+#include <boost/noncopyable.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <cstdlib>
+
+#include <wayland-client.h>
+#include <wayland-version.h>
+
+#include "windowing/DllWaylandClient.h"
+#include "windowing/DllWaylandEgl.h"
+#include "windowing/DllXKBCommon.h"
+#include "windowing/WaylandProtocol.h"
+
+#include "system.h"
+#include "guilib/gui3d.h"
+#include "utils/log.h"
+#include "EGLNativeTypeWayland.h"
+#include "windowing/WinEvents.h"
+
+#include "wayland/WaylandLibraries.h"
+#include "wayland/XBMCConnection.h"
+#include "wayland/XBMCSurface.h"
+
+namespace xw = xbmc::wayland;
+
+#if defined(HAVE_WAYLAND)
+class CEGLNativeTypeWayland::Private
+{
+public:
+
+  boost::scoped_ptr<xw::Libraries> m_libraries;
+  boost::scoped_ptr<xw::XBMCConnection> m_connection;
+  boost::scoped_ptr<xw::XBMCSurface> m_surface;
+
+  bool LoadWaylandLibraries();
+  void UnloadWaylandLibraries();
+};
+
+bool CEGLNativeTypeWayland::Private::LoadWaylandLibraries()
+{
+  try
+  {
+    m_libraries.reset(new xw::Libraries());
+  }
+  catch (const std::runtime_error &err)
+  {
+    CLog::Log(LOGWARNING, "%s: %s\n",
+              __FUNCTION__, err.what());
+    return false;
+  }
+  
+  return true;
+}
+
+void CEGLNativeTypeWayland::Private::UnloadWaylandLibraries()
+{
+  m_libraries.reset();
+}
+
+#else
+class CEGLNativeTypeWayland::Private
+{
+};
+#endif
+
+CEGLNativeTypeWayland::CEGLNativeTypeWayland() :
+  priv(new Private())
+{
+}
+
+CEGLNativeTypeWayland::~CEGLNativeTypeWayland()
+{
+} 
+
+bool CEGLNativeTypeWayland::CheckCompatibility()
+{
+#if defined(HAVE_WAYLAND)
+  if (!getenv("WAYLAND_DISPLAY"))
+  {
+    CLog::Log(LOGWARNING, "%s:, WAYLAND_DISPLAY is not set",
+              __FUNCTION__);
+    return false;
+  }
+  
+  /* FIXME:
+   * There appears to be a bug in DllDynamic::CanLoad() which causes
+   * it to always return false. We are just loading the library 
+   * directly at CheckCompatibility time now */
+  if (!priv->LoadWaylandLibraries())
+    return false;
+
+  return true;
+#else
+  return false;
+#endif
+}
+
+void CEGLNativeTypeWayland::Initialize()
+{
+}
+
+void CEGLNativeTypeWayland::Destroy()
+{
+#if defined(HAVE_WAYLAND)
+  priv->UnloadWaylandLibraries();
+#endif
+}
+
+bool CEGLNativeTypeWayland::CreateNativeDisplay()
+{
+#if defined(HAVE_WAYLAND)
+  try
+  {
+    xw::XBMCConnection::EventInjector injector =
+    {
+      CWinEventsWayland::SetWaylandDisplay,
+      CWinEventsWayland::DestroyWaylandDisplay,
+      CWinEventsWayland::MessagePump
+    };
+      
+    priv->m_connection.reset(new xw::XBMCConnection(priv->m_libraries->ClientLibrary(),
+                                                    priv->m_libraries->XKBCommonLibrary(),
+                                                    injector));
+  }
+  catch (const std::runtime_error &err)
+  {
+    CLog::Log(LOGERROR, "%s: %s", __FUNCTION__, err.what());
+    return false;
+  }
+
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool CEGLNativeTypeWayland::CreateNativeWindow()
+{
+#if defined(HAVE_WAYLAND)
+  try
+  {
+    priv->m_surface.reset(new xw::XBMCSurface(priv->m_libraries->ClientLibrary(),
+                                              priv->m_libraries->EGLLibrary(),
+                                              priv->m_connection->GetCompositor(),
+                                              priv->m_connection->GetShell(),
+                                              640,
+                                              480));
+  }
+  catch (const std::runtime_error &err)
+  {
+    CLog::Log(LOGERROR, "%s: %s", __FUNCTION__, err.what());
+    return false;
+  }
+
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool CEGLNativeTypeWayland::GetNativeDisplay(XBNativeDisplayType **nativeDisplay) const
+{
+#if defined(HAVE_WAYLAND)
+  /* We need to return a pointer to the wl_display * (eg wl_display **),
+   * as EGLWrapper needs to dereference our return value to get the
+   * actual display and not its first member */
+  *nativeDisplay =
+      reinterpret_cast <XBNativeDisplayType *>(priv->m_connection->NativeDisplay());
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool CEGLNativeTypeWayland::GetNativeWindow(XBNativeDisplayType **nativeWindow) const
+{
+#if defined(HAVE_WAYLAND)
+  *nativeWindow =
+      reinterpret_cast <XBNativeWindowType *>(priv->m_surface->EGLNativeWindow());
+  return true;
+#else
+  return false;
+#endif
+}  
+
+bool CEGLNativeTypeWayland::DestroyNativeDisplay()
+{
+#if defined(HAVE_WAYLAND)
+  priv->m_connection.reset();
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool CEGLNativeTypeWayland::DestroyNativeWindow()
+{
+#if defined(HAVE_WAYLAND)
+  priv->m_surface.reset();
+  return true;  
+#else
+  return false;
+#endif
+}
+
+bool CEGLNativeTypeWayland::GetNativeResolution(RESOLUTION_INFO *res) const
+{
+#if defined(HAVE_WAYLAND)
+  priv->m_connection->CurrentResolution(*res);
+
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool CEGLNativeTypeWayland::SetNativeResolution(const RESOLUTION_INFO &res)
+{
+#if defined(HAVE_WAYLAND)
+  priv->m_surface->Resize(res.iScreenWidth, res.iScreenHeight);
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool CEGLNativeTypeWayland::ProbeResolutions(std::vector<RESOLUTION_INFO> &resolutions)
+{
+#if defined(HAVE_WAYLAND)
+  priv->m_connection->AvailableResolutions(resolutions);
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool CEGLNativeTypeWayland::GetPreferredResolution(RESOLUTION_INFO *res) const
+{
+#if defined(HAVE_WAYLAND)
+  priv->m_connection->PreferredResolution(*res);
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool CEGLNativeTypeWayland::ShowWindow(bool show)
+{
+#if defined(HAVE_WAYLAND)
+  if (show)
+    priv->m_surface->Show();
+  else
+    return false;
+
+  return true;
+#else
+  return false;
+#endif
+}
